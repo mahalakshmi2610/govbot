6df20beec84bbcf188d970e12c5f2723d6276c62
mahalakshmi1326 <mahalakshmikumar2610@gmail.com>
2024-04-24 21:19:47 +0530

    Little css modification in App.css

--- src/Register.js
+++ src/Register.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-// import bcrypt from "bcryptjs";
 import chatImage from "./farmer.jpg";
 
 const Register = () => {
@@ -30,11 +29,9 @@ const Register = () => {
     e.preventDefault();
     if (!validatePassword()) return;
 
-    // const hashedPassword = bcrypt.hashSync(data.password.toLowerCase(), 10);
-
     const sendData = {
-        username: data.username,
-        password: data.password
+      username: data.username,
+      password: data.password
     };
 
     axios.post('http://localhost/govbotreactphp/insert.php', sendData)
@@ -53,43 +50,29 @@ const Register = () => {
   };
 
   return (
-    <div className="main-box">
-      <div className="image-container">
-        <img src={chatImage} alt="Registration" />
-      </div>
-      <form onSubmit={submitForm} className="form-container">
-        <div className="row">
-          <div className="col-md-12 text-center">
-            <h1>Register</h1>
-          </div>
-          <div className="row">
-            <div className="col-md-6">Username</div>
-            <div className="col-md-6">
-              <input type="text" name="username" className="form-control" onChange={handleChange} value={data.username} />
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-md-6">Password</div>
-            <div className="col-md-6">
-              <input type="password" name="password" className="form-control" onChange={handleChange} value={data.password} />
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-md-6">Confirm Password</div>
-            <div className="col-md-6">
-              <input type="password" name="confirmPassword" className="form-control" onChange={handleChange} value={data.confirmPassword} />
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-md-6 text-center">
-              <input type="submit" name="submit" value="Register" className="btn btn-success" />
-            </div>
-          </div>
-
-          {error && <div className="row"><div className="col-md-12 text-center text-danger">{error}</div></div>}
-
+    <div class="outer-box">
+      <div className="main-box">
+        <div className="image-container">
+          <img src={chatImage} alt="Registration" />
         </div>
-      </form>
+        <form onSubmit={submitForm} className="form-container">
+          <h1 id="regId">Register</h1>
+          <div className="form-group">
+            <label>Username</label>
+            <input type="text" name="username" className="form-control" onChange={handleChange} value={data.username} />
+          </div>
+          <div className="form-group">
+            <label>Password</label>
+            <input type="password" name="password" className="form-control" onChange={handleChange} value={data.password} />
+          </div>
+          <div className="form-group">
+            <label>Confirm Password</label>
+            <input type="password" name="confirmPassword" className="form-control" onChange={handleChange} value={data.confirmPassword} />
+          </div>
+          <button type="submit" className="btn btn-success">Register</button>
+          {error && <div className="text-danger">{error}</div>}
+        </form>
+      </div>
     </div>
   );
 };
